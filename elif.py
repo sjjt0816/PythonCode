d918ea14b9983b8f99f43037e09d406d07202f1c
sjjt0816 <jarve007@gmail.com>
2014-10-13 14:43:45 +0800

    elif.py

---
+++ elif.py
@@ -0,0 +1,11 @@
+x = input("what's your favorite fruit ? \n apple , banana , lemon , grape ? \n")
+if x == "apple":
+    print("it's yummy !!")
+elif x == "banana":
+    print("it's good !!")
+elif x == "lemon":
+    print("it's sour !!")
+elif x == "grape":
+    print("it's juicy !!")
+else :
+    print("what's your favorite fruit ?")
